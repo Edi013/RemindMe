1936ec966fe4dda0e9af091ac6671b960c343be7
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-11-13 08:40:16 +0200

    Add AppDbContext class and needed packages for entity in data access

---
+++ remind_me_be/RemindMe/RemindMe.DataAcces/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RemindMe.Domain.Entities;
+
+namespace RemindMe.DataAcces
+{
+    public class AppDbContext : DbContext
+    {
+        private IConfiguration Configuration;
+
+        public AppDbContext(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            base.OnConfiguring(options);
+
+            options.UseNpgsql(Configuration.GetConnectionString("RemindMeDb"));
+        }
+
+        public DbSet<ToDo> ToDos { get; set; }
+    }
+}
