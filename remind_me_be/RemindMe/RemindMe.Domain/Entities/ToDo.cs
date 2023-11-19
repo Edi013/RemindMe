90aa44791dd2c42b50951dcde6a3a0f9297efc33
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-11-20 00:04:41 +0200

    Add migration

--- remind_me_be/RemindMe/RemindMe.Domain/Entities/ToDo.cs
+++ remind_me_be/RemindMe/RemindMe.Domain/Entities/ToDo.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RemindMe.Domain.Entities
+﻿namespace RemindMe.Domain.Entities
 {
     public class ToDo
     {
