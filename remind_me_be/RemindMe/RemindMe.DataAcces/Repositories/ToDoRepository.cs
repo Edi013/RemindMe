127caf21c895775037d13909b6d4ea5f9588ae9e
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-11-20 00:04:31 +0200

    Adding Repository pattern : Interface - RepositoryBase - TableRepository

---
+++ remind_me_be/RemindMe/RemindMe.DataAcces/Repositories/ToDoRepository.cs
@@ -0,0 +1,13 @@
+﻿using RemindMe.Domain.Entities;
+
+namespace RemindMe.DataAcces.Repositories
+{
+    public class ToDoRepository : RepositoryBase<ToDo>
+    {
+        public ToDoRepository(ApplicationDbContext context)
+            : base (context)
+        {
+        }
+
+    }
+}
---
+++ remind_me_be/RemindMe/RemindMe.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace RemindMe.Domain.Interfaces
+{
+    public interface IRepository<T>
+    {
+        public Task<IEnumerable<T>> GetAll();
+        public Task<T> GetById(long id);
+        public Task<T> Add(T entity);
+        public Task<T> Update(T entity);
+        public Task Delete(T entity);
+        public Task<IEnumerable<T>> FindByCondition(Expression<Func<T, bool>> expression);
+        public Task<T> SingleByCondition(Expression<Func<T, bool>> expression);
+        public Task<bool> AnyByCondition(Expression<Func<T, bool>> expression);
+    }
+}
