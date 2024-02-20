42ab0f205eb824d14acab26d05fd40b73dabedec
Eduard Ghenea <87914375+Edi013@users.noreply.github.com>
2024-02-20 21:45:33 +0200

    home screen temporary drawer and bottom bar

--- remind_me_fe/lib/core/home/presentation/screens/home_screen.dart
+++ remind_me_fe/lib/core/home/presentation/screens/home_screen.dart
@@ -10,6 +10,33 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Screen'),
       ),
+      drawer: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth > constraints.maxHeight) {
+            return Drawer(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  const DrawerHeader(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                    ),
+                    child: Text('Drawer Header'),
+                  ),
+                  ListTile(
+                    title: Text('Item 1'),
+                    onTap: () {
+                      // Handle item tap
+                    },
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return const SizedBox();
+          }
+        },
+      ),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
@@ -17,6 +44,33 @@ class HomeScreen extends StatelessWidget {
           },
           child: const Text('Go to Todo List'),
         ),
+      ),
+      bottomNavigationBar: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth < constraints.maxHeight) {
+            return BottomNavigationBar(
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.business),
+                  label: 'Business',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.school),
+                  label: 'School',
+                ),
+              ],
+              currentIndex: 0,
+              selectedItemColor: Colors.blue,
+              onTap: (int index) {},
+            );
+          } else {
+            return const SizedBox.shrink();
+          }
+        },
       ),
     );
   }
