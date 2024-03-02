ee28b552575454420d7c7311427f15537bca21fb
Eduard Ghenea <87914375+Edi013@users.noreply.github.com>
2024-03-02 23:55:35 +0200

    renaming constants for themes name

--- remind_me_fe/lib/features/theme/data/local/current_app_theme.dart
+++ remind_me_fe/lib/features/theme/data/local/current_app_theme.dart
@@ -4,26 +4,26 @@ import 'package:remind_me_fe/core/constants.dart';
 class AppTheme {
   late String currentThemeName;
 
-  AppTheme({String currentTheme = system_theme}) {
+  AppTheme({String currentTheme = system_theme_name}) {
     currentThemeName = currentTheme;
   }
 
   ThemeMode buildAppThemeMode() {
     switch (currentThemeName) {
-      case light_theme:
+      case light_theme_name:
         return ThemeMode.light;
-      case dark_theme:
+      case dark_theme_name:
         return ThemeMode.dark;
-      case system_theme:
+      case system_theme_name:
         return ThemeMode.system;
     }
     throw AssertionError(error_message_constants_not_used_theme);
   }
 
   void swapTheme(String themeName) {
-    if (themeName != dark_theme &&
-        themeName != light_theme &&
-        themeName != system_theme) {
+    if (themeName != dark_theme_name &&
+        themeName != light_theme_name &&
+        themeName != system_theme_name) {
       throw AssertionError(error_message_constants_not_used_theme);
     }
     currentThemeName = themeName;
