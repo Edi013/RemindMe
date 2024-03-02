636f9dbaa7b661d192c445279f24618ff729c8df
Eduard Ghenea <87914375+Edi013@users.noreply.github.com>
2024-03-02 23:56:39 +0200

    Introduce mechanism for retrieving active todos

--- remind_me_fe/lib/core/constants.dart
+++ remind_me_fe/lib/core/constants.dart
@@ -6,9 +6,9 @@ const String BASE_URL_FLUTTER_CLIENT = "http://localhost:51742";
 
 const String DATE_TIME_FORMAT = "yyyy-MM-ddTHH:mm";
 
-const String light_theme = "light";
-const String dark_theme = "dark";
-const String system_theme = "system";
+const String light_theme_name = "light";
+const String dark_theme_name = "dark";
+const String system_theme_name = "system";
 const String error_message_constants_not_used_theme =
     "Theme name doesn't use one of the allowed constants for theme name !";
 
--- remind_me_fe/lib/features/todos/presentation/providers/todo_provider.dart
+++ remind_me_fe/lib/features/todos/presentation/providers/todo_provider.dart
@@ -9,20 +9,37 @@ class TodoProvider extends ChangeNotifier {
 
   TodoProvider(TodoRepositoryImpl repo) {
     repository = repo;
-    //initialize();
   }
-
-  // void initialize() async {
-  //   todos = await getAll();
-  //   notifyListeners();
-  // }
 
   Future<List<TodoEntity>> getAll() async {
     var result = await repository.getAll();
     todos = result;
     notifyListeners();
-    filterActiveTodos();
     return result;
+  }
+
+  Future<void> add(TodoEntity object) async {
+    await repository.addTodo(object).then((TodoEntity value) {
+      todos.add(value);
+      notifyListeners();
+      obtainActiveTodosFromAllTodos();
+    });
+  }
+
+  Future<void> update(int index, TodoEntity updatedObject) async {
+    await repository.updateTodo(updatedObject).then((TodoEntity value) {
+      todos[index] = value;
+      notifyListeners();
+      obtainActiveTodosFromAllTodos();
+    });
+  }
+
+  Future<void> delete(int id) async {
+    await repository.deleteTodo(id).then((value) {
+      todos.remove(todos.firstWhere((element) => element.id == id));
+      notifyListeners();
+      obtainActiveTodosFromAllTodos();
+    });
   }
 
   Future<List<TodoEntity>> getAllActiveTodos() async {
@@ -32,36 +49,15 @@ class TodoProvider extends ChangeNotifier {
     return result;
   }
 
-  void filterActiveTodos() {
+  Future<void> obtainActiveTodosFromAllTodos() async {
     final now = DateTime.now();
+    if (todos.isEmpty) {
+      await getAll();
+    }
     activeTodos = todos
         .where(
             (todo) => todo.startDate.isBefore(now) && todo.endDate.isAfter(now))
         .toList();
     notifyListeners();
-  }
-
-  Future<void> add(TodoEntity object) async {
-    await repository.addTodo(object).then((TodoEntity value) {
-      todos.add(value);
-      notifyListeners();
-      filterActiveTodos();
-    });
-  }
-
-  Future<void> update(int index, TodoEntity updatedObject) async {
-    await repository.updateTodo(updatedObject).then((TodoEntity value) {
-      todos[index] = value;
-      notifyListeners();
-      filterActiveTodos();
-    });
-  }
-
-  Future<void> delete(int id) async {
-    await repository.deleteTodo(id).then((value) {
-      todos.remove(todos.firstWhere((element) => element.id == id));
-      notifyListeners();
-      filterActiveTodos();
-    });
   }
 }
